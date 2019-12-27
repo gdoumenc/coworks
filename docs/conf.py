9574546ec9e08de13aa6df9b6992e00ab211dcd7
Guillaume Doumenc <gdoumenc@fpr-coworks.com>
2019-12-27 13:34:34 +0100

    master doc to index.rst

--- docs/conf.py
+++ docs/conf.py
@@ -15,6 +15,9 @@
 # sys.path.insert(0, os.path.abspath('.'))
 
 
+# The master toctree document.
+master_doc = 'index'
+
 # -- Project information -----------------------------------------------------
 
 project = 'coworks'
@@ -23,7 +26,6 @@ author = 'gdoumenc'
 
 # The full version, including alpha/beta/rc tags
 release = '0.2.0'
-
 
 # -- General configuration ---------------------------------------------------
 
@@ -40,7 +42,6 @@ templates_path = ['_templates']
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
 
 # -- Options for HTML output -------------------------------------------------
 
