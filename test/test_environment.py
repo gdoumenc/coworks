436c9e4aea7b1967c8faefa723c4b524cd80130e
Guillaume Doumenc <gdoumenc@fpr-coworks.com>
2020-01-03 17:43:31 +0100

    stage environment in test

---
+++ test/test_environment.py
@@ -0,0 +1,30 @@
+import requests
+import os
+
+from .microservice import *
+
+
+class WithEnvMS(SimpleMS):
+
+    def get(self):
+        """Root access."""
+        return os.getenv("test")
+
+
+def test_default(local_server_factory):
+    local_server = local_server_factory(WithEnvMS(), config_path="test/example")
+    response = local_server.make_call(requests.get, '/')
+    assert response.status_code == 200
+    assert response.text == 'test environment variable'
+
+def test_dev_stage(local_server_factory):
+    local_server = local_server_factory(WithEnvMS(), config_path="test/example", stage="dev")
+    response = local_server.make_call(requests.get, '/')
+    assert response.status_code == 200
+    assert response.text == 'test environment variable'
+
+def test_prod_stage(local_server_factory):
+    local_server = local_server_factory(WithEnvMS(), config_path="test/example", stage="master")
+    response = local_server.make_call(requests.get, '/')
+    assert response.status_code == 200
+    assert response.text == 'prod variable'
