94f3fbe0babdc9a6a5536786f903b1f83c5cb904
lrocher <lrocher@holding-fak.com>
2020-03-23 15:51:43 +0100

    add test on attachments

--- test/tech/test_mail.py
+++ test/tech/test_mail.py
@@ -1,7 +1,7 @@
 from unittest.mock import MagicMock
-
 import pytest
 import requests
+import os
 
 from coworks.tech import MailMicroService
 
@@ -14,17 +14,29 @@ env = {
 
 @pytest.mark.local
 class ATestMail:
-
     def test_send(self, local_server_factory, smtp_mock_fixture, email_mock_fixture):
         local_server = local_server_factory(MailMicroService(env=env))
+
+        text_attachment = open("attachment.txt", "w+")
+        text_attachment.write("This is the content of the attachemnt")
+        text_attachment.close()
+        text_attachment = open("attachment.txt", "r")
+
         response = local_server.make_call(requests.post, '/send',
-                                          json={'subject': "Test mail",
-                                                'from_addr': "myself@test.com",
-                                                'to_addrs': ["you@test.com", "and@test.com"],
-                                                'body': "content"})
+                                          params={'subject': "Test mail",
+                                                  'from_addr': "myself@test.com",
+                                                  'to_addrs': ["you@test.com", "and@test.com"],
+                                                  'body': "content"},
+                                          files={
+                                              'file1': ('attachment.txt', text_attachment, 'text/plain'),
+                                          })
+        text_attachment.close()
+        os.remove("attachment.txt")
         assert response.status_code == 200
         smtp_mock_fixture.login.assert_called_once_with("myself@test.com", "passwd")
         email_mock_fixture.set_content.assert_called_once_with('content')
+        email_mock_fixture.add_attachment.assert_called_with(b'This is the content of the attachemnt',
+                                                             filename='attachment.txt', maintype='text', subtype='plain')
         assert email_mock_fixture["Subject"] == "Test mail"
         assert email_mock_fixture["From"] == "myself@test.com"
         assert email_mock_fixture["To"] == "you@test.com, and@test.com"
