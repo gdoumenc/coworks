d6812782ae23206a0f3ff94c2e280e6494589cbc
lrocher <lambert.rocher@gmail.com>
2020-06-26 16:47:42 +0200

    add local deployer

--- coworks/cws/__init__.py
+++ coworks/cws/__init__.py
@@ -1,9 +1,12 @@
 from .writer import CwsTerraformWriter
 from .runner import CwsRunner
+from .deployer import CwsDeployer
+
 
 class CwsProject():
 
     def __init__(self, app=None):
         CwsRunner(app)
         CwsTerraformWriter(app, name='export')
+        CwsDeployer(app)
 
--- coworks/cws/layers.py
+++ coworks/cws/layers.py
@@ -1,28 +1,29 @@
 import importlib.util
 import os
 import re
+import shutil
+
 from functools import partial
 from pathlib import Path
 from typing import Optional, Iterable
 from zipfile import ZipFile
+from subprocess import run
+from tempfile import TemporaryDirectory
 
 from SCons import Environment
 from SCons.Builder import Builder
 
-DEFAULT_MODULES = [
-    'coworks',
-    'chalice', 'aws_xray_sdk', 'jsonpickle', 'wrapt',
-    'jinja2', 'yaml', 'markupsafe',
-    'requests', 'chardet', 'certifi', 'idna', 'requests_toolbelt', 'zeep',
-]
 EXCLUDE_FILE_PATTERN = r".*.py[co]"
 
 
 class Layer:
 
-    def __init__(self, layer_zipfilename: str, modules: Optional[Iterable] = None,
+    def __init__(self, layer_zipfilename: str,
+                 modules: Optional[Iterable] = None,
                  source_dirs: Optional[Iterable] = None,
-                 exclude_dirs: Optional[Iterable] = None, exclude_file_pattern: str = None):
+                 exclude_dirs: Optional[Iterable] = None,
+                 exclude_file_pattern: str = None,
+                 requirements_path: str = None):
         """
         Create a zip file for AWS Layer.
         :param layer_zipfilename: zip file name.
@@ -33,10 +34,11 @@ class Layer:
         :return: None.
         """
         self.__source_dirs = source_dirs or []
-        self.__modules = modules if modules is not None else DEFAULT_MODULES
+        self.__modules = modules
         self.__exclude_dirs = exclude_dirs or []
         self.__exclude_file_pattern = exclude_file_pattern
         self.__exclude_regexp = None
+        self.__requirements_path = requirements_path
 
         env = Environment.Base(BUILDERS={'Layer': Builder(action=partial(self.generate_zip_file, target=self))})
 
@@ -46,13 +48,29 @@ class Layer:
         dependencies = []
         for source_dir in self.__source_dirs:
             self.walk(add_dep, source_dir)
-        for module in self.__modules:
-            spec = importlib.util.find_spec(module)
-            if spec:
-                if spec.submodule_search_locations:
-                    for location in spec.submodule_search_locations:
-                        self.walk(add_dep, location)
-        env.Layer(layer_zipfilename, [file.as_posix() for file in dependencies])
+
+        if self.__modules:
+            for module in self.__modules:
+                spec = importlib.util.find_spec(module)
+                if spec:
+                    if spec.submodule_search_locations:
+                        for location in spec.submodule_search_locations:
+                            self.walk(add_dep, location)
+            print([file.as_posix() for file in dependencies])
+            env.Layer(layer_zipfilename, [file.as_posix() for file in dependencies])
+        else:
+            for zip_filename in layer_zipfilename:
+                Layer.generate_layer_from_requirements(zip_filename, self.__requirements_path)
+
+    @staticmethod
+    def generate_layer_from_requirements(zip_filename, requirements_path):
+        with TemporaryDirectory() as temp_dir:
+            python_temp_dir = os.path.join(temp_dir, 'python')
+            os.makedirs(python_temp_dir)
+            run(f"pip install -r {requirements_path} -t {python_temp_dir}", shell=True)
+            if zip_filename[-4:] == '.zip':
+                zip_filename = zip_filename[:-4]  # shutil.make_archive automatically adds .zip extension
+            shutil.make_archive(zip_filename, 'zip', temp_dir)
 
     @property
     def exclude_regexp(self):
@@ -66,6 +84,7 @@ class Layer:
     def generate_zip_file(self, target, source, env=None):
         for t in target:
             zip_path = os.path.join(t.dir.get_abspath(), t.name)
+
             with ZipFile(zip_path, mode='w') as layer_zip:
                 def add_zip(root, parent, file, zip_prefix=None):
                     dirs = Path(parent).relative_to(root)
