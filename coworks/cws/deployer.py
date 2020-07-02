a678743e87a08a6f4d1b5c3700eb9d036bc8e34b
lrocher <lambert.rocher@gmail.com>
2020-07-02 17:40:39 +0200

    add local deployer command

--- coworks/cws/deployer.py
+++ coworks/cws/deployer.py
@@ -1,6 +1,6 @@
-import os
-
 import click
+import os
+import subprocess
 
 from .command import CwsCommand
 
@@ -17,70 +17,32 @@ class CwsDeployer(CwsCommand):
         )
 
     def _execute(self, *, workspace, project_dir='.', debug=True, **kwargs):
-        print(self.app.config)
-        print("deploy")
+        CwsDeployer._local_deploy(workspace, debug, project_dir, **kwargs)
 
-    def _local_deploy(self, deploy_config, workspace, debug):
+    @staticmethod
+    def _local_deploy(workspace, debug, project_dir, module=None, service=None, **kwargs):
         from python_terraform import Terraform
-        print("deploy")
+        terraform = Terraform(working_dir=os.path.join('.', 'terraform'))
+        CwsDeployer._terraform_export_and_apply_local(terraform, project_dir, module, service, workspace, '', debug)
+        CwsDeployer._terraform_export_and_apply_local(terraform, project_dir, module, service, workspace, workspace, debug)
 
+    @staticmethod
+    def _terraform_export_and_apply_local(terraform, project_dir, module, service, workspace, stage, debug):
+        tf_file_content = subprocess.check_output(
+            f"cws -p {project_dir} -m {module} -s {service} terraform-staging --stage={stage} --workspace={workspace}",
+            shell=True).decode('utf-8')
+        with open(os.path.join(".", "terraform", f"_{module}-{service}.tf"), 'w') as tf_file:
+            tf_file.write(tf_file_content)
+        CwsDeployer._terraform_apply_local(terraform, "default", debug)
+        CwsDeployer._terraform_apply_local(terraform, workspace, debug)
+
+    @staticmethod
+    def _terraform_apply_local(terraform, workspace, debug):
+        return_code, stdout, stderr = terraform.workspace('select', workspace)
+        if workspace != 'default' and return_code != 0:
+            terraform.workspace('new', workspace)
+        terraform.init(input=False)
+        return_code, stdout, stderr = terraform.apply(skip_plan=True, input=False)
+        # return_code, stdout, stderr = terraform.plan(input=False)
         if debug:
-            print("Local deployment")
-
-        try:
-            deploy_config = deploy_config['deploy']
-        except KeyError:
-            raise Exception("Configuration file does not contain section 'deploy'")
-
-        try:
-            services_config = deploy_config['services']
-        except KeyError:
-            raise Exception("Configuration file does not contain section 'services' in the section 'deploy'")
-
-        services_to_deploy_config = [service_config for service_config in services_config if
-                                     service_config['workspace'] == workspace]
-
-        if debug:
-            print(f"Preparing to deploy the following services : {services_to_deploy_config}")
-
-        for service_to_deploy_config in services_to_deploy_config:
-
-            terraform_variables = {'environment_variables_file': service_to_deploy_config['environment_variables_file'],
-                                   'custom-layers': service_to_deploy_config.get('custom-layers'),
-                                   'common-layers': service_to_deploy_config.get('common-layers'),
-                                   'stage': ''}
-
-            if debug:
-                print("Create terraform configuration")
-            t = Terraform(working_dir=os.path.join('.', deploy_config['terraform']))
-
-            if debug:
-                print("Deploying")
-
-            t.workspace('select', 'default')
-            t.init(input=False)
-            return_code, stdout, stderr = t.apply(skip_plan=True, input=False, var=terraform_variables)
-            if debug:
-                print(return_code, stdout, stderr)
-
-            return_code, stdout, stderr = t.workspace('select', service_to_deploy_config['workspace'])
-            if return_code != 0:
-                t.workspace('new', service_to_deploy_config['workspace'])
-            t.init(input=False)
-            return_code, stdout, stderr = t.apply(skip_plan=True, input=False, var=terraform_variables)
-            if debug:
-                print(return_code, stdout, stderr)
-
-            terraform_variables['stage'] = service_to_deploy_config['workspace']
-
-            t.workspace('select', 'default')
-            return_code, stdout, stderr = t.apply(skip_plan=True, input=False, var=terraform_variables)
-            if debug:
-                print(return_code, stdout, stderr)
-
-            return_code, stdout, stderr = t.workspace('select', service_to_deploy_config['workspace'])
-            if return_code != 0:
-                t.workspace('new', service_to_deploy_config['workspace'])
-            return_code, stdout, stderr = t.apply(skip_plan=True, input=False, var=terraform_variables)
-            if debug:
-                print(return_code, stdout, stderr)
+            print(return_code, stdout, stderr)
