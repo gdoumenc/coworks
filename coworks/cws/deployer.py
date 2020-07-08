04f7e4c3ac1f3a32704295ac2c5022647dd7fb61
gdo <gdoumenc@fpr-coworks.com>
2020-07-08 12:06:04 +0200

    deploeyer command enahnced

--- coworks/cws/deployer.py
+++ coworks/cws/deployer.py
@@ -1,9 +1,45 @@
 import os
+import sys
 
 import click
 from python_terraform import Terraform
 
 from .command import CwsCommand
+
+
+class CwsTerraform(Terraform):
+
+    def __init__(self, working_dir, debug):
+        super().__init__(working_dir=working_dir, terraform_bin_path='/usr/local/bin/terraform')
+        self.debug = debug
+        self.__initialized = False
+
+    def apply_local(self, workspace):
+        self.select_workspace(workspace)
+        if not self.__initialized:
+            self.init()
+            self.__initialized = True
+        self.apply()
+
+    def select_workspace(self, workspace):
+        return_code, out, err = self.workspace('select', workspace)
+        self._print(out, err)
+        if workspace != 'default' and return_code != 0:
+            return_code, out, err = self.workspace('new', workspace)
+            self._print(out, err)
+
+    def init(self, **kwargs):
+        return_code, out, err = super().init(input=False, raise_on_error=True)
+        self._print(out, err)
+
+    def apply(self, **kwargs):
+        return_code, out, err = super().apply(skip_plan=True, input=False, raise_on_error=True)
+        self._print(out, err)
+
+    def _print(self, out, err):
+        if self.debug:
+            print(out, file=sys.stdout)
+            print(err, file=sys.stderr)
 
 
 class CwsDeployer(CwsCommand):
@@ -28,35 +64,23 @@ class CwsDeployer(CwsCommand):
         pass
 
     def _local_deploy(self, options):
+        """ Deploiement in 4 steps:
+        create
+            Step 1. Create API
+            Step 2. Create Lambda
+        update
+            Step 3. Update API integration
+            Step 4. Update API deployment
         """
-        Step 1. Create API
-        2. Create Lambda
-        3. API integration
-        4. Deploy API
-        :param options:
-        :return:
-        """
-        terraform = Terraform(working_dir=os.path.join('.', 'terraform'))
 
-        self._terraform_export_and_apply_local(terraform, 'create', options)
-        self._terraform_export_and_apply_local(terraform, 'update', options)
+        self._terraform_export_and_apply_local('create', options)
+        self._terraform_export_and_apply_local('update', options)
 
-    def _terraform_export_and_apply_local(self, terraform, step, options):
+    def _terraform_export_and_apply_local(self, step, options):
         output_file = os.path.join(".", "terraform", f"_{options.module}-{options.service}.tf")
         self.app.execute('terraform-staging', output=output_file, step=step, **options.to_dict())
 
         if not options['dry']:
-            debug = options['debug']
-            terraform_apply_local(terraform, "default", debug)
-            terraform_apply_local(terraform, options.workspace, debug)
-
-
-def terraform_apply_local(terraform, workspace, debug):
-    return_code, stdout, stderr = terraform.workspace('select', workspace)
-    if workspace != 'default' and return_code != 0:
-        terraform.workspace('new', workspace)
-    terraform.init(input=False)
-    return_code, stdout, stderr = terraform.apply(skip_plan=True, input=False)
-    # return_code, stdout, stderr = terraform.plan(input=False)
-    if debug:
-        print(return_code, stdout, stderr)
+            terraform = CwsTerraform(os.path.join('.', 'terraform'), options['debug'])
+            terraform.apply_local("default")
+            terraform.apply_local(options.workspace)
