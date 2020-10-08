0cce88b1a928054ed3ef64afa6efe3af90003b3d
gdoumenc <gdoumenc@fpr-coworks.com>
2020-10-08 12:02:12 +0200

    json body and query parameters added to xrays middleware trace

--- coworks/middleware/xray_middleware.py
+++ coworks/middleware/xray_middleware.py
@@ -2,6 +2,7 @@ from functools import partial, update_wrapper
 
 from aws_xray_sdk.core.models import http
 
+LAMBDA_NAMESPACE = 'lambda'
 COWORKS_NAMESPACE = 'coworks'
 
 
@@ -13,20 +14,20 @@ class XRayMiddleware(object):
 
         self._recorder = recorder
 
-        @self.app.before_first_activation
+        @app.before_first_activation
         def capture_routes(event, context):
-            for path, route in self.app.routes.items():
+            for path, route in app.routes.items():
                 for method, entry in route.items():
                     view_function = entry.view_function
                     cws_function = view_function.__cws_func__
 
                     def captured(_view_function, *args, **kwargs):
                         subsegment = self._recorder.current_subsegment()
-                        subsegment.put_metadata('event', event, COWORKS_NAMESPACE)
-                        subsegment.put_metadata('context', context, COWORKS_NAMESPACE)
-                        subsegment.put_annotation('service', self.app.name)
-                        subsegment.put_metadata('args', args, COWORKS_NAMESPACE)
-                        subsegment.put_metadata('kwargs', kwargs, COWORKS_NAMESPACE)
+                        subsegment.put_metadata('event', event, LAMBDA_NAMESPACE)
+                        subsegment.put_metadata('context', context, LAMBDA_NAMESPACE)
+                        subsegment.put_annotation('service', app.name)
+                        subsegment.put_metadata('query_params', app.current_request.query_params, COWORKS_NAMESPACE)
+                        subsegment.put_metadata('json_body', app.current_request.json_body, COWORKS_NAMESPACE)
                         response = _view_function(*args, **kwargs)
                         subsegment.put_metadata('response', response, COWORKS_NAMESPACE)
                         return response
