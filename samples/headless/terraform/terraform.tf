74851b2bb1a6ec2deb96557d407aa224bfb6583c
gdoumenc <gdoumenc@fpr-coworks.com>
2021-01-18 16:47:05 +0100

    better headless sample

--- samples/headless/terraform/terraform.tf
+++ samples/headless/terraform/terraform.tf
@@ -8,24 +8,7 @@ locals {
 }
 
 resource "aws_s3_bucket_object" "assets" {
-  for_each = fileset(path.module, "../assets/**")
-  bucket = local.samples_bucket
-  key = format("${local.sample_name}/%s", trimprefix(each.value, "../"))
-  source = each.key
-  etag = filemd5(each.key)
-  content_type = lookup(var.file_types, regexall("\\.[^\\.]+\\z", each.key)[0], "application/octet-stream")
-  acl = "public-read"
-  tags = {
-    Infra = "project"
-    Name = local.sample_name
-    Creator = "terraform"
-    Target = "bucket"
-    Stage = "sample"
-  }
-}
-
-resource "aws_s3_bucket_object" "images" {
-  for_each = fileset(path.module, "../images/**")
+  for_each = fileset(path.module, "../{assets,images}/**")
   bucket = local.samples_bucket
   key = format("${local.sample_name}/%s", trimprefix(each.value, "../"))
   source = each.key
@@ -50,6 +33,20 @@ resource "aws_cloudfront_distribution" "coworks-headless" {
     origin_path = "/${local.sample_name}"
   }
 
+  origin {
+    domain_name = "${aws_api_gateway_rest_api.website-handless[0].id}.execute-api.eu-west-1.amazonaws.com"
+    origin_id = "API-${local.samples_bucket}/${local.sample_name}"
+    origin_path = "/dev"
+    custom_origin_config {
+      http_port = 80
+      https_port = 443
+      origin_protocol_policy = "https-only"
+      origin_ssl_protocols = ["TLSv1"]
+    }
+  }
+
+  aliases = ["*.morassuti.com", "morassuti.com"]
+  price_class = "PriceClass_All"
   enabled = true
   is_ipv6_enabled = true
 
@@ -60,18 +57,11 @@ resource "aws_cloudfront_distribution" "coworks-headless" {
     }
   }
 
-  //  aliases = ["morassuti.com", "morassuti.fr"]
-
   default_cache_behavior {
     allowed_methods = ["GET", "HEAD"]
     cached_methods = ["GET", "HEAD"]
-    target_origin_id = "S3-${local.samples_bucket}/${local.sample_name}"
-
-    viewer_protocol_policy = "allow-all"
-    default_ttl = 0
-
-    max_ttl = 86400
-
+    target_origin_id = "API-${local.samples_bucket}/${local.sample_name}"
+    viewer_protocol_policy = "redirect-to-https"
     forwarded_values {
       query_string = false
       cookies {
@@ -80,8 +70,31 @@ resource "aws_cloudfront_distribution" "coworks-headless" {
     }
   }
 
+  dynamic "ordered_cache_behavior" {
+    for_each = ["assets", "images"]
+    content {
+      allowed_methods = ["GET", "HEAD"]
+      cached_methods = ["GET", "HEAD"]
+      target_origin_id = "S3-${local.samples_bucket}/${local.sample_name}"
+      path_pattern = "${ordered_cache_behavior.value}/*"
+
+      viewer_protocol_policy = "redirect-to-https"
+      default_ttl = 0
+
+      max_ttl = 86400
+
+      forwarded_values {
+        query_string = false
+        cookies {
+          forward = "none"
+        }
+      }
+    }
+  }
+
   viewer_certificate {
-    cloudfront_default_certificate = true
+    acm_certificate_arn = "arn:aws:acm:us-east-1:935392763270:certificate/7907fef7-23c3-4ae2-900c-59fd8389ce95"
+    ssl_support_method = "sni-only"
   }
 
   tags = {
