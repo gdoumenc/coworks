5681309847b8ac6a828105637d2ae404964f140d
gdoumenc <gdoumenc@fpr-coworks.com>
2021-06-16 10:19:43 +0200

    jinja version fixed

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
         'aws_xray_sdk>=2.5.0',
         'boto3>=1.13.15',
         'chalice>=1.14.1',
-        'jinja2>=2.11',
+        'jinja2==2.11.0',
         'pyyaml>=5.3.1',
         'requests_toolbelt>=0.9.1',
         'python_terraform>=0.10.1'
