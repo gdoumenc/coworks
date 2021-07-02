e9278c5e2fd60a1fdc3c2b669c4171683d016e42
gdoumenc <gdoumenc@fpr-coworks.com>
2021-07-02 18:40:18 +0200

    proxy redesigned

--- coworks/proxy.py
+++ coworks/proxy.py
@@ -34,20 +34,20 @@ class MicroServiceProxy:
         })
         self.url = f"https://{self.cws_id}.execute-api.eu-west-1.amazonaws.com/{self.cws_stage}/"
 
-    def get(self, path, data=None, response_content_type='json'):
+    def get(self, path, params=None, response_content_type='json'):
         if path.startswith('/'):
             path = path[1:]
-        resp = self.session.get(urljoin(self.url, path), data=data)
+        resp = self.session.get(urljoin(self.url, path), params=params)
         return self.convert(resp, response_content_type)
 
     # noinspection PyShadowingNames
-    def post(self, path, data=None, json=None, headers=None, sync=True, response_content_type='json'):
+    def post(self, path, json=None, headers=None, sync=True, response_content_type='json'):
         if path.startswith('/'):
             path = path[1:]
         headers = {**self.session.headers, **headers} if headers else self.session.headers
         if not sync:
             headers.update({'InvocationType': 'Event'})
-        resp = self.session.post(urljoin(self.url, path), data=data, json=json or {}, headers=headers)
+        resp = self.session.post(urljoin(self.url, path), json=json or {}, headers=headers)
         return self.convert(resp, response_content_type)
 
     @property
