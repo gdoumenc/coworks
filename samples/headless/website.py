e3594e3beadc476f334616182ed5ed5e5e228915
gdoumenc <gdoumenc@fpr-coworks.com>
2021-07-15 09:01:13 +0200

    authorization-value option for runner sample headless enhanced

--- samples/headless/website.py
+++ samples/headless/website.py
@@ -28,12 +28,18 @@ class WebsiteMicroService(TechMicroService):
 
     @entry
     def get(self):
+        """Entry for the home page."""
         template_filename = 'home.j2'
         template = self.jinja_env.get_template(template_filename)
-        return self.render(template, **self.home)
+
+        response = self.cosmic_client.object('home')
+        home = self.cosmic_client.fields(response)
+
+        return self.render(template, **home)
 
     @entry
     def get_biere(self, name=None):
+        """Entry for the beer (all or one specific)."""
         template_filename = 'one-beer.j2' if name else 'all-beer.j2'
         template = self.jinja_env.get_template(template_filename)
 
@@ -45,24 +51,20 @@ class WebsiteMicroService(TechMicroService):
 
     @entry
     def get_assets(self, folder, filename):
+        """Access for all assets."""
         file = Path.cwd() / 'assets' / folder / filename
-        return self.get_file_content(file)
+        mt = mimetypes.guess_type(file)
+        content = file.read_bytes()
+        try:
+            return content.decode('utf-8'), 200, {'Content-Type': mt[0]}
+        except UnicodeDecodeError:
+            return content, 200, {'Content-Type': mt[0]}
 
     @entry
     def get_form(self):
         template_filename = 'form.j2'
         template = self.jinja_env.get_template(template_filename)
         return self.render(template)
-
-    @property
-    def home(self):
-        response = self.cosmic_client.object('home')
-        home = self.cosmic_client.fields(response)
-        return {
-            'carousel': home.get('carousel'),
-            'sections': home.get('sections'),
-            # 'footer': home.get('footer'),
-        }
 
     def get_beers(self, name=None):
         response = self.cosmic_client.objects('bieres')
@@ -80,19 +82,10 @@ class WebsiteMicroService(TechMicroService):
         headers = {'Content-Type': 'text/html; charset=utf-8'}
         return template.render(assets_url=assets_url, root='.', beers=beers, **data), 200, headers
 
-    @staticmethod
-    def get_file_content(file: Path):
-        mt = mimetypes.guess_type(file)
-        content = file.read_bytes()
-        try:
-            return content.decode('utf-8'), 200, {'Content-Type': mt[0]}
-        except:
-            return content, 200, {'Content-Type': mt[0]}
-
 
 app = WebsiteMicroService()
 app.register_blueprint(Admin(), url_prefix='admin')
 
 if __name__ == '__main__':
-    app.execute("run", project_dir='.', module='website', workspace='local', service='app', auto_reload=True,
-                authorization='test')
+    app.execute("run", project_dir='.', module='website', workspace='local', auto_reload=True,
+                authorization_value='test')
