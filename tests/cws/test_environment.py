3bd5597fac360a454492d6b16dd7753465e1874f
gdoumenc <gdoumenc@fpr-coworks.com>
2021-12-07 19:35:10 +0100

    prod version may uppercase

--- tests/cws/test_environment.py
+++ tests/cws/test_environment.py
@@ -65,13 +65,31 @@ class TestClass:
         assert response.text == "Value of environment variable test is : test dev environment variable."
         server.terminate()
 
-    def test_run_prod_stage(self, example_dir, unused_tcp_port):
+    def test_run_prod1_stage(self, example_dir, unused_tcp_port):
         config_dev = Config(environment_variables_file=Path("config") / "vars.dev.json")
         config_prod = ProdConfig(environment_variables_file=Path("config") / "vars.prod.json")
         app = EnvTechMS(configs=[config_dev, config_prod], root_path=example_dir)
         app.any_token_authorized = True
         server = multiprocessing.Process(target=run_server_with_workspace,
                                          args=(example_dir, app, unused_tcp_port, "v1"),
+                                         daemon=True)
+        server.start()
+        counter = 1
+        time.sleep(counter)
+        while not server.is_alive() and counter < 3:
+            time.sleep(counter)
+            counter += 1
+        response = requests.get(f'http://localhost:{unused_tcp_port}/env', headers={'Authorization': "token"})
+        assert response.text == "Value of environment variable test is : test prod environment variable."
+        server.terminate()
+
+    def test_run_prod2_stage(self, example_dir, unused_tcp_port):
+        config_dev = Config(environment_variables_file=Path("config") / "vars.dev.json")
+        config_prod = ProdConfig(environment_variables_file=Path("config") / "vars.prod.json")
+        app = EnvTechMS(configs=[config_dev, config_prod], root_path=example_dir)
+        app.any_token_authorized = True
+        server = multiprocessing.Process(target=run_server_with_workspace,
+                                         args=(example_dir, app, unused_tcp_port, "V1"),
                                          daemon=True)
         server.start()
         counter = 1
