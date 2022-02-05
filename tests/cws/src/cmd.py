a1b0746782935e078c134490ce958e5e3f1697ce
gdoumenc <gdoumenc@fpr-coworks.com>
2022-02-05 17:53:20 +0100

    project config file with workspaces

--- tests/cws/src/cmd.py
+++ tests/cws/src/cmd.py
@@ -11,12 +11,26 @@ from tests.cws.src.app import EnvTechMS
 @click.pass_context
 @pass_script_info
 def cmd(info, ctx, a, b):
-    app = info.load_app()
-    assert app is not None
-    assert app.config is not None
+    cws_app = info.load_app()
+    assert cws_app is not None
+    assert cws_app.config is not None
     if a:
         print(f"test command with a={a}/", end='')
     print(f"test command with b={b}", end='', flush=True)
+
+
+@click.command("test", short_help="Test custom command.")
+@click.option('-a', required=True)
+@click.option('--b')
+@click.pass_context
+@pass_script_info
+def cmd1(info, ctx, a, b):
+    cws_app = info.load_app()
+    assert cws_app is not None
+    assert cws_app.config is not None
+    if a:
+        print(f"test command v1 with a={a}/", end='')
+    print(f"test command v1 with b={b}", end='', flush=True)
 
 
 app = EnvTechMS()
