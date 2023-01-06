07808195a9f9857f8fcc80f3842dcebc40c161a8
gdoumenc <gdoumenc@fpr-coworks.com>
2023-01-06 18:34:17 +0100

    cleaning unused code preparing biz creation

--- coworks/biz/__init__.py
+++ coworks/biz/__init__.py
@@ -9,7 +9,20 @@ from coworks.biz.operators import TechMicroServiceOperator
 from coworks.biz.sensors import AsyncTechMicroServiceSensor
 from coworks.biz.sensors import TechMicroServiceSensor
 
-biz = dag
+
+def biz(*biz_args, **biz_kwargs):
+    """dag decorator redefined to create the tech microservice for triggering it in the future."""
+
+    if 'doc_md' in biz_kwargs:
+        biz_kwargs['doc_md'] += """
+<img style="margin-bottom:auto;width:100px;" src="https://neorezo.io/assets/img/logo_neorezo.png"/>
+"""
+
+    def wrapper(func):
+        return dag(*biz_args, **biz_kwargs)(func)
+
+    return wrapper
+
 
 _all__ = (
     AsyncTechServicePullOperator, BranchTechMicroServiceOperator, TechMicroServiceOperator,
