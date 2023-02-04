b24c3646fec3b1f1310d2fc78d84bed8b1804612
gdoumenc <gdoumenc@fpr-coworks.com>
2023-02-04 12:54:53 +0100

    no need project context issue fixed

--- coworks/cws/command.py
+++ coworks/cws/command.py
@@ -2,14 +2,15 @@ from click import Command
 from click import UsageError
 
 def no_project_context(f):
+    """Decorator to allow command without need to have a project dir defined."""
     setattr(f, '__need_project_context', False)
     return f
 
 class CwsCommand(Command):
 
     def invoke(self, ctx):
-        if getattr(self.callback, '__need_project_context', True) and self._context_project_dir(ctx):
-            raise UsageError(f"Project dir {self._context_project_dir(ctx)} not found.")
+        if getattr(self.callback, '__need_project_context', True) and not self._context_project_dir(ctx):
+            raise UsageError(f"Project dir {self._context_project_dir(ctx)} not defined.")
         return super().invoke(ctx)
 
     @staticmethod
